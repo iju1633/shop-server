e8cc98e80edd8a741e6b33b875bf860de34e7838
Garfield <iju1633@gmail.com>
2023-07-02 01:20:43 +0900

    config 구성

---
+++ src/main/java/com/global/shopserver/config/CorsConfig.java
@@ -0,0 +1,43 @@
+package com.global.shopserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class CorsConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()));
+
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ src/main/java/com/global/shopserver/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.global.shopserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(this.swaggerInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.global.shopserver.controller")) // request handler selector 가 스캔할 패키지 선택
+                .paths(PathSelectors.any()) // 모든 url 에 대해 명세서 작성
+                .build()
+                .useDefaultResponseMessages(true);
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("Shop-server API Documentation")
+                .description("SpringBoot로 개발한 웹 프로젝트입니다.")
+                .version("1.0")
+                .build();
+    }
+}
