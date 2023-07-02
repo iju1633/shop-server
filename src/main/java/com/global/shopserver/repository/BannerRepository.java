78915cfbbd4e2f9b80f08f283a3fc72264b6b582
Garfield <iju1633@gmail.com>
2023-07-02 19:09:15 +0900

    빠진 annotation 추가

--- src/main/java/com/global/shopserver/repository/BannerRepository.java
+++ src/main/java/com/global/shopserver/repository/BannerRepository.java
@@ -2,7 +2,9 @@ package com.global.shopserver.repository;
 
 import com.global.shopserver.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BannerRepository extends JpaRepository<Banner, Long> {
 
     Banner findBannerById(Long bannerId);
